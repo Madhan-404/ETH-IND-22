43506e36889ac25dcef320529ac356cb9e711a0f
SANTHOSH17-DOT <santhoshm12052003@gmail.com>
2022-12-04 00:38:01 +0530

    Added: infura provider for wallet connect feature

--- src/App.js
+++ src/App.js
@@ -1,75 +1,19 @@
 /** @format */
 
 import "@rainbow-me/rainbowkit/dist/index.css";
-import {
-  getDefaultWallets,
-  RainbowKitProvider,
-  darkTheme,
-} from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
-import { alchemyProvider } from "wagmi/providers/alchemy";
-import { BrowserRouter, Routes, Route, useNavigation } from "react-router-dom";
 import FilesDisplay from "./pages/FilesDisplay";
 import Home from "./pages/Home";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import styles from "./styles.module.css";
-import { useEffect, useState } from "react";
-import Web3 from "web3";
-
+import { useAccount } from "wagmi";
 export default function App() {
-  const [isConnected, setIsConnected] = useState(false);
-  useEffect(() => {
-    const checkConnection = async () => {
-      let web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-      const accounts = await web3.eth.getAccounts();
-      if (!accounts.length) {
-        setIsConnected(false);
-      } else {
-        setIsConnected(true);
-      }
-    };
-    checkConnection();
-    console.log("isConnected = ", isConnected);
-  }, []);
-  const { chains, provider } = configureChains(
-    [
-      chain.mainnet,
-      chain.polygon,
-      chain.goerli,
-      chain.polygonMumbai,
-      // chain.hardhat,
-    ],
-    [alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_ID })]
-  );
-
-  const { connectors } = getDefaultWallets({
-    appName: "My RainbowKit App",
-    chains,
-  });
-
-  const wagmiClient = createClient({
-    autoConnect: true,
-    connectors,
-    provider,
-  });
+  const { isConnected } = useAccount();
   return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains} theme={darkTheme()}>
-        <div className={styles.App}>
-          <BrowserRouter>
-            <Navbar />
-            <Routes>
-              {!isConnected ? (
-                <Route path="/" element={<Home />} />
-              ) : (
-                <Route path="/files" element={<FilesDisplay />} />
-              )}
-            </Routes>
-            <Footer />
-          </BrowserRouter>
-        </div>
-      </RainbowKitProvider>
-    </WagmiConfig>
+    <div className={styles.App}>
+      <Navbar />
+      {isConnected ? <FilesDisplay /> : <Home />}
+      <Footer />
+    </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,52 @@
+/** @format */
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  darkTheme,
+} from "@rainbow-me/rainbowkit";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
 import App from "./App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+const { chains, provider } = configureChains(
+  [
+    chain.mainnet,
+    chain.polygon,
+    chain.goerli,
+    chain.polygonMumbai,
+    // chain.hardhat,
+  ],
+  [
+    infuraProvider({
+      apiKey: process.env.REACT_APP_INFURA_APIKEY,
+      priority: 0,
+    }),
+    // publicProvider({ priority: 1 }),
+  ]
+);
 
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 root.render(
   <StrictMode>
-    <App />
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} theme={darkTheme()}>
+        <App />
+      </RainbowKitProvider>
+    </WagmiConfig>
   </StrictMode>
 );

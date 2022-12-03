93424478e65e6f44dc06793b3028794af0cdf6b4
K V Madhan Raj <83569612+Madhan-404@users.noreply.github.com>
2022-12-03 11:30:42 +0530

    walletadded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "@rainbow-me/rainbowkit/dist/index.css";
+import { getDefaultWallets, RainbowKitProvider,darkTheme } from "@rainbow-me/rainbowkit";
+import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+import { YourComponent } from "./Components/YourComponent";
 
-function App() {
+const { chains, provider } = configureChains(
+  [chain.mainnet, chain.polygon,chain.goerli, chain.polygonMumbai,chain.hardhat],
+  [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "My RainbowKit App",
+  chains
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+});
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains} theme={darkTheme()}>
+        <YourComponent />
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
-
-export default App;
---
+++ src/Components/YourComponent.js
@@ -0,0 +1,17 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+
+export const YourComponent = () => {
+  return (
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <ConnectButton />
+    </div>
+  );
+};
